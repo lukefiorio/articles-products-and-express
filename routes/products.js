4bd72ee329fd138be8913e077fa852398bf00c4d
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-26 20:14:16 -1000

    working redirect for products

--- routes/products.js
+++ routes/products.js
@@ -5,11 +5,12 @@ const productDB = require('./../db/products.js');
 routerProducts
   .route('/')
   .get((req, res) => {
-    productDB.emptyMessage();
     res.render('templates/products/index', productDB.retrieveAll());
+    productDB.removeMessage();
   })
   .post((req, res) => {
-    res.render('templates/products/index', productDB.create(req.body));
+    productDB.create(req.body);
+    res.redirect('/products');
   });
 
 routerProducts.route('/new').get((req, res) => {
@@ -20,18 +21,35 @@ routerProducts
   .route('/:productId')
   .get((req, res) => {
     const id = req.url.slice(1);
-    productDB.emptyMessage();
-    res.render(`templates/products/${productDB.retrieveOne(id).returnPage}`, productDB.retrieveOne(id));
+    const findProduct = productDB.getIndex(id);
+    if (findProduct === -1) {
+      return res.redirect('/products');
+    }
+    // if last message on specific product was for failed edit ...
+    // then remove the message when routing to "/products/:id" ...
+    // since message was displayed originally on re-route to "/products"
+    if (productDB.getMessage(id) === '{ "success": false, "message": "No field values were provided" }') {
+      productDB.removeMessage(id);
+    }
+    res.render(`templates/products/product`, productDB.retrieveOne(id));
+    productDB.removeMessage(id);
   })
   .put((req, res) => {
     const endIndexQmark = req.url.indexOf('?', 1) - 1;
     const id = req.url.substr(1, endIndexQmark);
-    res.render('templates/products/product', productDB.update(req.body, id));
+    productDB.update(req.body, id);
+    const errorMsg = productDB.getMessage(id).indexOf('"success": false') > -1;
+    if (errorMsg) {
+      res.redirect('/products');
+    } else {
+      res.redirect(`/products/${id}`);
+    }
   })
   .delete((req, res) => {
     const endIndexQmark = req.url.indexOf('?', 1) - 1;
     const id = req.url.substr(1, endIndexQmark);
-    res.render('templates/products/index', productDB.remove(id));
+    productDB.remove(id);
+    res.redirect('/products');
   });
 
 routerProducts.route('/:productId/edit').get((req, res) => {
